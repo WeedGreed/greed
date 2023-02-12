5e917c76975f6392ee9c3a8f17f3eac4e9ef64df
yacho yves <yachoyves@gmail.com>
2023-02-12 23:36:58 +0000

    comments

--- src/main/java/weed/greed/Test.java
+++ src/main/java/weed/greed/Test.java
@@ -1,6 +1,10 @@
+/**
+    comment hi guy
+ */
 package weed.greed;
 public class Test{
     public static void main(String[] args){
+        //hi what is your name?
         System.out.println("hello!");
     }
 }
